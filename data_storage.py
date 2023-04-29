f5b7ec0b043f52c090b8c32a1c5e60cb0f481d61
Packan <ariaxero@gmail.com>
2023-04-29 20:37:51 +0200

    ETL works and now displays through matplotlib

--- data_storage.py
+++ data_storage.py
@@ -2,9 +2,10 @@ import os
 import json
 
 # Save raw weather data as a JSON file in the specified folder.
-def save_raw_data(data, filename, folder='raw'):
-    os.makedirs(folder, exist_ok=True)
-    file_path = os.path.join(folder, filename)
+def save_data(data, filename, folder='raw', data_type='raw'):
+    folder_path = os.path.join(folder, data_type)
+    os.makedirs(folder_path, exist_ok=True)
+    file_path = os.path.join(folder_path, filename)
 
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from weather_api import fetch_weather_data
-from data_storage import save_raw_data
+from data_storage import save_data
 from data_processing import harmonize_weather_data
-
+from data_conversion import harmonized_data_to_dataframe, pd
+from data_visualization import plot_weather_data
 
 API_KEY = "f287254d7f65d11a9be865eeb88e798f"
-LOCATION = "Stockholm"
+LOCATIONS = ["Stockholm", "Gothenburg", "Malmo", "Uppsala", "Vasteras"]
 
 def main():
-    raw_weather_data = fetch_weather_data(API_KEY, LOCATION)
-    save_raw_data(raw_weather_data, f"{LOCATION}_weather.json")
+    all_weather_data = []
 
-    harmonized_data = harmonize_weather_data(raw_weather_data)
-    print(harmonized_data)
+    for location in LOCATIONS:
+        raw_weather_data = fetch_weather_data(API_KEY, location)
+        save_data(raw_weather_data, f"{location}_weather.json", data_type='raw')
 
+        harmonized_data = harmonize_weather_data(raw_weather_data)
+        save_data(harmonized_data, f"{location}_harmonized_weather.json", data_type='harmonized')
+
+        weather_dataframe = harmonized_data_to_dataframe(harmonized_data)
+        all_weather_data.append(weather_dataframe)
+
+    combined_weather_data = pd.concat(all_weather_data, ignore_index=True)
+    plot_weather_data(combined_weather_data)
 if __name__ == "__main__":
     main()
